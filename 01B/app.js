c8150fa203226b0b697dc3d4f9c8c8964bd0fecb
Maaike Seijsener <36770@ma-web.nl>
2023-11-28 14:28:48 +0100

    stap1

--- 01B/app.js
+++ 01B/app.js
@@ -3,23 +3,23 @@ class App
     runApplication()
     {
         this.greeting = "Starting up"
-        let appnaam = "Demoapp"
-        let versienummer = "0.1"
-        let versiedatum = "21-11-2023"
-        let Autheur = "Maaike"
-        let copyright = "Maaike Bv"
-        let distributeur = "Maaike Soft"
-        let darkmode = "False"
+        this.appnaam = "Demoapp"
+        this.versienummer = "0.1"
+        this.versiedatum = "21-11-2023"
+        this.Autheur = "Maaike"
+        this.copyright = "Maaike Bv"
+        this.distributeur = "Maaike Soft"
+        this.darkmode = "False"
     }
 }
 
 let app = new App();
 app.runApplication();
 console.log(app.greeting);
-console.log("appNaam: " +app.Demoapp);
-console.log("versienummer " + app."0.1");
-console.log("versiedatum " + app.);
-console.log("Autheur " + app.Maaike);
-console.log("copyright " + app.Maaike Bv);
-console.log("distributeur " + app.Maaike Soft);
-console.log("darkmode " + app.False);
+console.log("appNaam: " + app.appnaam);
+console.log("versienummer: " + app.versienummer);
+console.log("versiedatum: " + app.versiedatum);
+console.log("Autheur: " + app.Autheur);
+console.log("copyright: " + app.copyright);
+console.log("distributeur: " + app.distributeur);
+console.log("darkmode: " + app.False);
---
+++ 02 canvashuis/app.js
@@ -0,0 +1,12 @@
+class App
+{
+    runApplication()
+    {
+      let canvas= document.getElementById("canvasId")
+      console.log(canvas)
+    }
+}
+
+let app = new App();
+app.runApplication();
+console.log()
