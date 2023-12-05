b64d7d1544ee35e2347cd958a674380e04813839
Maaike Seijsener <36770@ma-web.nl>
2023-12-05 13:14:52 +0100

    ifelse

---
+++ 03 ifelse/app.js
@@ -0,0 +1,17 @@
+class App
+{
+    runApplication()
+    {
+     console.log("Hello world.");
+     /// hier komt de code ///
+     /// dit is de code comentaar///
+     /// dit is ook comentaar///
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+/// <script type="txt/javascript" src="app.js"></script>///
+/// <script type="module" defer src="app.js"></script> ///
+
