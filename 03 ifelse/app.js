7f72b91feab28756d362e1f5d641409bd1185d16
Maaike Seijsener <36770@ma-web.nl>
2023-12-07 15:10:41 +0100

    ifels

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -2,18 +2,38 @@ class App
 {
     runApplication()
     {
-     console.log("Hello world.");
-     let title = document.getElementById("newstitle");
-     console.log(title);
-     /// hier komt de code ///
-     /// dit is de code comentaar///
-     /// dit is ook comentaar///
+        console.log("hello world!");
+        ///code gaat hier!///
+        let title = document.getElementById("newstitle");
+        console.log(title);
+        let newsitem1 = document.getElementsByClassName("gamenews")[0]
+        let newsitem2 = document.getElementsByClassName("gamenews")[1]
+        console.log(newsitem1);
+        console.log(newsitem2);
+        let random = Math.random();
+        console.log(random);
+        if(random < 0.2){
+            title.style.backgroundColor = "FF0000";
+        } else if(random < 0.6 && random > 0.2){
+            title.style.backgroundColor = "345FF9"
+        } else if(random < 0.75 && random > 0.6){
+            title.style.backgroundColor = "746783"
+        } else {
+            title.style.backgroundColor = "586648"
+        }
+        if(random < 0.5){
+            newsitem1.style.backgroundColor = "87EFEA"
+        } else{
+            newsitem1.style.backgroundColor = "F6A7B0"  
+        }
+        if(random < 0.33){
+            newsitem2.style.backgroundColor = "A4BB77"
+        } else if (random > 0.33 && random < 0.67){
+            newsitem2.style.backgroundColor = "EEBBAA"
+        } else if (random > 0.67 && random < 1){
+            newsitem2.style.backgroundColor = "F6A7F8"
+        }
     }
 }
-
 let app = new App();
 app.runApplication();
-
-/// <script type="txt/javascript" src="app.js"></script>///
-/// <script type="module" defer src="app.js"></script> ///
-
